8d148628a52dcd0c14ee8ad6d0062655168605e6
Walter Tay Ann Lee <40448679+watate@users.noreply.github.com>
2025-02-26 00:13:23 +0800

    Add ARM64 code

--- sample-apps/layer-python/layer/1-install.sh
+++ sample-apps/layer-python/layer/1-install.sh
@@ -1,3 +1,5 @@
-python3.11 -m venv create_layer
+python3.10 -m venv create_layer
 source create_layer/bin/activate
+# For ARM64
+# pip install -r requirements.txt --platform=manylinux2014_aarch64 --only-binary=:all: --target ./create_layer/lib/python3.10/site-packages
 pip install -r requirements.txt
